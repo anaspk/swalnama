8a52792c95ce7fdec265b5bb9d77d17bcab6942c
Muhammad Anas <anastts.pk@gmail.com>
2012-12-23 20:58:44 +0500

    Complete the interactive behavior of the question form.

--- js/questionForm.js
+++ js/questionForm.js
@@ -1,3 +1,5 @@
+var optionCount = 4;
+
 $(function() {
     //alert("Hurrayyy!!! me loaded ... :)");
     $questionType = $('#Question_questionType');
@@ -9,5 +11,20 @@ $(function() {
     
     $questionType.change(function(){
         $questionOptions.toggle( 'blind' );
+    });
+    
+    $addOptionButton = $('#add-option-btn');
+    
+    $addOptionButton.click(function($e){
+        $e.preventDefault();
+        optionCount++;
+        var $controlGroup = $('<div/>', {'class':'control-group'});
+        var $controlLabel = $('<label/>', {'class':'control-label', 'for':'options['+ optionCount + ']', 'text':'Option ' + optionCount});
+        var $controls = $('<div/>', {'class':'controls'});
+        var $input = $('<input/>', {'class':'span8', 'type':'text'})
+        $controls.append($input);
+        $controlGroup.append($controlLabel);
+        $controlGroup.append($controls);
+        $controlGroup.insertBefore($('#add-option-btn-group'));
     });
 });
--- protected/views/question/_form.php
+++ protected/views/question/_form.php
@@ -18,38 +18,27 @@
 <?php echo $form->dropDownListRow($model, 'questionType', $model->questionTypes, array('class'=>'span8')); ?>
 <?php Yii::app()->clientScript->registerScriptFile('js/questionForm.js', CClientScript::POS_END); ?>
 <div id="questionOptionsBlock">
-    <?php //echo $form->textFieldRow($model->options[0], 'optionStatement'); ?>
-    <?php //echo $form->textFieldRow($model->options[1], 'optionStatement'); ?>
-    <?php //echo $form->textFieldRow($model->options[2], 'optionStatement'); ?>
-    <?php //echo $form->textFieldRow($model->options[3], 'optionStatement'); ?>
-    <div class="control-group">
-        <label class="control-label" for="options[1]">Option 1</label>
-        <div class="controls">
-            <input class="span8" type="text" id="options[1]" />
+    <?php for ( $i=1; $i<=4; $i++): ?>
+        <div class="control-group">
+            <label class="control-label" for="options[<?php echo $i; ?>]">Option <?php echo $i; ?></label>
+            <div class="controls">
+                <input class="span8" type="text" id="options[<?php echo $i; ?>]" />
+            </div>
         </div>
-    </div>
-    <div class="control-group">
-        <label class="control-label" for="options[2]">Option 2</label>
-        <div class="controls">
-            <input class="span8" type="text" id="options[2]" />
-        </div>
-    </div>
-    <div class="control-group">
-        <label class="control-label" for="options[3]">Option 3</label>
-        <div class="controls">
-            <input class="span8" type="text" id="options[3]" />
-        </div>
-    </div>
-    <div class="control-group">
-        <label class="control-label" for="options[4]">Option 4</label>
-        <div class="controls">
-            <input class="span8" type="text" id="options[4]" />
+    <?php endfor; ?>
+    <div class="control-group" id="add-option-btn-group">
+        <div class="controls" >
+            <?php $this->widget('bootstrap.widgets.TbButton', array(
+                'buttonType' => 'link',
+                'type' => 'success',
+                'label' => 'Add another option',
+                'url' => '#',
+                'id' => 'add-option-btn'
+            )); ?>
         </div>
     </div>
     <?php echo $form->checkboxRow($model, 'multipleChoiceAllowed'); ?>
 </div>
-
-
 <div class="form-actions">
         <?php $this->widget('bootstrap.widgets.TbButton', array(
             'buttonType'=>'submit',
