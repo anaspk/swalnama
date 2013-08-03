6613b8f4e455cbd4536525c8c92e4a73113ad454
Muhammad Anas <anastts.pk@gmail.com>
2013-08-03 23:21:45 +0500

    Revert the Contact Us form to Yii default in the default theme

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -23,43 +23,63 @@ $this->breadcrumbs=array(
 If you need help or have any other kind of queries, please fill out the following form to contact us. Thank you.
 </p>
 
-<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'contact-form',
-        'type' => 'horizontal',
 	'enableClientValidation'=>true,
 	'clientOptions'=>array(
 		'validateOnSubmit'=>true,
 	),
-        'htmlOptions' => array(
-            'class' => 'well',
-        ),
 )); ?>
 
-<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
-<?php echo $form->errorSummary($model); ?>
-<?php echo $form->textFieldRow($model,'name', array('class'=>'span8')); ?>
-<?php echo $form->textFieldRow($model,'email', array('class'=>'span8')); ?>
-<?php echo $form->textFieldRow($model,'subject', array('class'=>'span8', 'size'=>60,'maxlength'=>128)); ?>
-<?php echo $form->textAreaRow($model,'body', array('class'=>'span8', 'rows'=>6, 'cols'=>50)); ?>
+	<?php echo $form->errorSummary($model); ?>
 
-<?php if(CCaptcha::checkRequirements()): ?>
-    <?php echo $form->captchaRow($model, 'verifyCode'); ?>
-<?php endif; ?>
+	<div class="row">
+		<?php echo $form->labelEx($model,'name'); ?>
+		<?php echo $form->textField($model,'name'); ?>
+		<?php echo $form->error($model,'name'); ?>
+	</div>
 
-<div class="form-actions">
-    <?php $this->widget('bootstrap.widgets.TbButton', array(
-        'buttonType' => 'submit',
-        'type' => 'primary',
-        'label' => 'Submit',
-    )); ?>
-    <?php $this->widget('bootstrap.widgets.TbButton', array(
-        'buttonType' => 'link',
-        'url' => Yii::app()->user->returnUrl,
-        'label' => 'Cancel',
-    )); ?>
-</div>
+	<div class="row">
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email'); ?>
+		<?php echo $form->error($model,'email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'subject'); ?>
+		<?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($model,'subject'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'body'); ?>
+		<?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model,'body'); ?>
+	</div>
+
+        <?php if(CCaptcha::checkRequirements()): ?>
+	<div class="row">
+		<?php echo $form->labelEx($model,'verifyCode'); ?>
+		<div>
+		<?php $this->widget('CCaptcha'); ?>
+		<?php echo $form->textField($model,'verifyCode'); ?>
+		</div>
+		<div class="hint">Please enter the letters as they are shown in the image above.
+		<br/>Letters are not case-sensitive.</div>
+		<?php echo $form->error($model,'verifyCode'); ?>
+	</div>
+	<?php endif; ?>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Submit'); ?>
+	</div>
 
 <?php $this->endWidget(); ?>
+
+</div><!-- form -->
 
 <?php endif; ?>
